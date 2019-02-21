e82d53f446cf56a41eeb6ec5943b1bf31ca474ff
zhaohengxu <1354280240@qq.com>
2019-02-21 09:46:36 +0800

    shop

--- app/Http/Controllers/Weixin/WeixinController.php
+++ app/Http/Controllers/Weixin/WeixinController.php
@@ -169,12 +169,10 @@ class WeixinController extends Controller
     {
         //获取缓存
         $token = Redis::get($this->redis_weixin_access_token);
-        print_r($token);die;
         if(!$token){        // 无缓存 请求微信接口
             $url = 'https://api.weixin.qq.com/cgi-bin/token?grant_type=client_credential&appid='.env('WEIXIN_APPID').'&secret='.env('WEIXIN_APPSECRET');
             $data = json_decode(file_get_contents($url),true);
 
-            print_r($data);die;
             //记录缓存
             $token = $data['access_token'];
             Redis::set($this->redis_weixin_access_token,$token);
@@ -309,6 +307,21 @@ class WeixinController extends Controller
         echo $this->getWXAccessToken();
     }
 
-
+    /**
+     * 自动回复
+     */
+    public function getContent($openid,$toUserName){
+        $time=time();
+        $date=date("Y/m/d H:i:s");
+        $content='你好，我是狸狸狸！温馨提示您当前时间为'.$date;
+        $xmlStrResopnse='<xml>
+            <ToUserName><![CDATA['.$openid.']]></ToUserName>
+            <FromUserName><![CDATA['.$toUserName.']]></FromUserName>
+            <CreateTime>'.time().'</CreateTime>
+            <MsgType><![CDATA[text]]></MsgType>
+            <Content><![CDATA['.$content.']]></Content>
+            </xml>';
+        echo $xmlStrResopnse;
+    }
 
 }
