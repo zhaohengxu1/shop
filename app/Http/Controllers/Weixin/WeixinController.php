e26c97fae93aa6430975ff0209ee698e0c09846c
zhaohengxu <1354280240@qq.com>
2019-02-25 10:07:57 +0800

    reply

--- app/Http/Controllers/Weixin/WeixinController.php
+++ app/Http/Controllers/Weixin/WeixinController.php
@@ -36,18 +36,18 @@ class WeixinController extends Controller
             $openid = $xml_str->FromUserName;
             //获取用户微信信息
             $toUserName = $xml_str->ToUserName;
-//            //用户发送文字
-//            if ($xml_str->MsgType == 'text') {
-//                $msg = $xml_str->Content;
-//                $xmlStrResopnse = '<xml>
-//                <ToUserName><![CDATA[' . $openid . ']]></ToUserName>
-//                <FromUserName><![CDATA[' . $toUserName . ']]></FromUserName>
-//                <CreateTime>' . time() . '</CreateTime>
-//                <MsgType><![CDATA[text]]></MsgType>
-//                <Content><![CDATA[' . $msg . ']]></Content>
-//                </xml>';
-//                echo $xmlStrResopnse;
-//            }
+            //用户发送文字
+            if ($xml_str->MsgType == 'text') {
+                $msg = $xml_str->Content;
+                $xmlStrResopnse = '<xml>
+                <ToUserName><![CDATA[' . $openid . ']]></ToUserName>
+                <FromUserName><![CDATA[' . $toUserName . ']]></FromUserName>
+                <CreateTime>' . time() . '</CreateTime>
+                <MsgType><![CDATA[text]]></MsgType>
+                <Content><![CDATA[' . $msg . ']]></Content>
+                </xml>';
+                echo $xmlStrResopnse;
+            }
 
             if(isset($xml_str->MsgType)){
                 if ($xml_str->MsgType == 'text') {            //用户发送文本消息
